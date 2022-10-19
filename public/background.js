f2f0ca0903fb21e28d68c3097eef34c9d50664d5
jasneet <jasneet.randhawa@growthnatives.com>
2022-10-19 18:40:30 +0530

    extension started

---
+++ public/background.js
@@ -0,0 +1,20 @@
+// chrome.runtime.onInstalled.addListener(function() {
+//     alert('You just made the best decision of today, by installing GMass!\n\nWe will now redirect you to your Gmail account so you can get started sending email campaigns inside Gmail.');
+  
+//     chrome.tabs.create({
+//       url: 'https://mail.google.com',
+//       active: true
+//     });
+  
+//     return false;
+//   });
+
+// chrome.runtime.onInstalled.addListener(function() {
+  
+//     chrome.tabs.create({
+//       url: 'https://mail.google.com',
+//       active: true
+//     });
+  
+//     return false;
+//   });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+ 
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <div className="App">
+     <h1>digg extension</h1>
+ <button onClick={()=>
+  {
+    chrome.tabs.create({
+            url: 'https://mail.google.com',
+            active: true
+          })
+         }
+  
+  }>Login/Signup</button>
+    
+   </div>
+ );
 }
-
+ 
 export default App;
